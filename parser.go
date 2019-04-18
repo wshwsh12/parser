ce81cfded25672f7c3ffe32234f0941dcb035f07
Ryan-Git <ryan.hd.ren@gmail.com>
2019-04-18 10:51:34 +0800

    parser: fix now frac in column option (#214)

--- parser.go
+++ parser.go
@@ -7594,8 +7594,7 @@ yynewstate:
 		}
 	case 101:
 		{
-			nowFunc := &ast.FuncCallExpr{FnName: model.NewCIStr("CURRENT_TIMESTAMP")}
-			parser.yyVAL.item = &ast.ColumnOption{Tp: ast.ColumnOptionOnUpdate, Expr: nowFunc}
+			parser.yyVAL.item = &ast.ColumnOption{Tp: ast.ColumnOptionOnUpdate, Expr: yyS[yypt-0].expr}
 		}
 	case 102:
 		{
